48a0d2ad005a2ea6c334021f8e67a9354716852c
Jesper Christiansen <jchristiansen@zendesk.com>
2015-02-02 09:55:35 -0800

    Moved code to MinitestConverter::Converters::Shoulda

--- lib/minitest_converter.rb
+++ lib/minitest_converter.rb
@@ -10,69 +10,15 @@ module MinitestConverter
       @path = path
       @output = []
       # @known_word_replacements = YAML::load_file = "known_word_replacements.yml"
-      @known_word_replacements = {}
       @stdin = stdin
     end
 
     def convert!
       content = File.read(@path)
-      replaced = convert_class_definition(content)
-      replaced = convert_context_to_describe(replaced)
-      replaced = convert_should_to_it(replaced)
-      replaced = convert_setup_to_before(replaced)
+      replaced = MinitestConverter::Converters::Shoulda.convert!(content)
 
       File.open(@path, 'w') { |file| file.write(replaced) }
       p "Done!"
-
-      File.open("known_word_replacements.yml", "w") do |file|
-        file.write @known_word_replacements.to_yaml
-      end
-    end
-
-    private
-
-    def convert_class_definition(content)
-      if content.match(/class (\S+) < ActiveSupport::TestCase/)
-        puts 'Converting class definition.....'
-        class_name = $1
-        puts "Class name (#{class_name})"
-        class_name_input = @stdin.gets.to_s.chomp.strip
-
-        content.gsub!("class #{class_name} < ActiveSupport::TestCase", "describe #{class_name_input} do")
-      end
-      content
-    end
-
-    def convert_context_to_describe(content)
-      content.gsub!('context', 'describe')
-      content
-    end
-
-    def convert_setup_to_before(content)
-      content.gsub!(/setup do/, 'before do')
-      content.gsub!(/setup {/, 'before {')
-      content
-    end
-
-    def convert_should_to_it(content)
-      matches = content.scan(/should ["'](.+)["'] do/)
-
-      matches.each do |match|
-        existing = match[0]
-        first_word = existing.split(' ').first
-        unless @known_word_replacements.key?(first_word)
-          puts "Rewrite first word (#{existing})"
-          substitute = @stdin.gets.to_s.chomp.strip || existing
-          unless first_word == substitute
-            @known_word_replacements[first_word] = substitute
-            content.gsub!(match[0], existing.gsub(first_word, substitute))
-          end
-        else
-          content.gsub!(match[0], existing.gsub(first_word, @known_word_replacements[first_word]))
-        end
-      end
-      content.gsub!('should', 'it')
-      content
     end
   end
 end
--- lib/minitest_converter/converters/shoulda.rb
+++ lib/minitest_converter/converters/shoulda.rb
@@ -1,7 +1,75 @@
 module MinitestConverter
   module Converters
     class Shoulda
+      attr_reader :content
 
+      def self.convert!(content)
+        new(content, STDIN).convert
+      end
+
+      def initialize(content, stdin=STDIN)
+        @content = content
+        @stdin = stdin
+        @known_word_replacements = {}
+      end
+
+      def convert
+        convert_class_definition
+        convert_context_to_describe
+        convert_should_to_it
+        convert_setup_to_before
+
+        File.open("known_word_replacements.yml", "w") do |file|
+          file.write @known_word_replacements.to_yaml
+        end
+
+        @content
+      end
+
+      private
+
+      def convert_class_definition
+        if @content.match(/class (\S+) < ActiveSupport::TestCase/)
+          puts 'Converting class definition.....'
+          class_name = $1
+          puts "Class name (#{class_name})"
+          class_name_input = @stdin.gets.to_s.chomp.strip
+
+          @content.gsub!("class #{class_name} < ActiveSupport::TestCase", "describe #{class_name_input} do")
+        end
+      end
+
+      def convert_context_to_describe
+        @content.gsub!('context', 'describe')
+        @content
+      end
+
+      def convert_setup_to_before
+        @content.gsub!(/setup do/, 'before do')
+        @content.gsub!(/setup {/, 'before {')
+        @content
+      end
+
+      def convert_should_to_it
+        matches = @content.scan(/should ["'](.+)["'] do/)
+
+        matches.each do |match|
+          existing = match[0]
+          first_word = existing.split(' ').first
+          unless @known_word_replacements.key?(first_word)
+            puts "Rewrite first word (#{existing})"
+            substitute = @stdin.gets.to_s.chomp.strip || existing
+            unless first_word == substitute
+              @known_word_replacements[first_word] = substitute
+              @content.gsub!(match[0], existing.gsub(first_word, substitute))
+            end
+          else
+            @content.gsub!(match[0], existing.gsub(first_word, @known_word_replacements[first_word]))
+          end
+        end
+        @content.gsub!('should', 'it')
+        @content
+      end
     end
   end
 end
